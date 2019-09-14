9eacca8d423c8f9cc9e2ca4d28ed23e779f2db57
世界因你而美好 <chinalollipop@gmail.com>
2019-09-14 11:32:31 +0800

    修熬过

--- HGAPP_A04/app/src/main/java/com/hg3366/a3366/common/http/Client.java
+++ HGAPP_A04/app/src/main/java/com/hg3366/a3366/common/http/Client.java
@@ -34,7 +34,7 @@ public class Client {
     private static OkHttpClient client;
     private static ClientConfig clientConfig;
     private static ProxyCallFactory proxyCallFactory;
-    public static String domainUrl = "https://suna01.com/";
+    public static String domainUrl = "https://hg3366aa.com/";
     /**
      * 应该在Application onCreate中使用
      * @param config
