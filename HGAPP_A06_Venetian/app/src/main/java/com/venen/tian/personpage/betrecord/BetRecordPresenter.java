b490158b6c5f5c5312ab277b501340d305841ceb
世界因你而美好 <chinalollipop@gmail.com>
2019-10-27 10:41:33 +0800

    修好

--- HGAPP_A06_Venetian/app/src/main/java/com/venen/tian/personpage/betrecord/BetRecordPresenter.java
+++ HGAPP_A06_Venetian/app/src/main/java/com/venen/tian/personpage/betrecord/BetRecordPresenter.java
@@ -42,6 +42,52 @@ public class BetRecordPresenter implements BetRecordContract.Presenter {
     }
 
     @Override
+    public void postBetAGDZRecordList(String appRefer, String gtype, String Checked, String Cancel, String date_start, String date_end, String page) {
+        subscriptionHelper.add(RxHelper.addSugar(api.postBetAGDZRecordList(HGConstant.PRODUCT_PLATFORM, gtype, Checked, Cancel, date_start, date_end, page))
+                .subscribe(new ResponseSubscriber<AppTextMessageResponse<BetRecordResult>>() {
+                    @Override
+                    public void success(AppTextMessageResponse<BetRecordResult> response) {
+                        if (response.isSuccess()) {
+                            view.postBetRecordResult(response.getData());
+                        } else {
+                            view.showMessage(response.getDescribe());
+                        }
+                    }
+
+                    @Override
+                    public void fail(String msg) {
+                        if (null != view) {
+                            view.setError(0, 0);
+                            view.showMessage(msg);
+                        }
+                    }
+                }));
+    }
+
+    @Override
+    public void postBetODZRecordList(String appRefer, String gtype, String Checked, String Cancel, String date_start, String date_end, String page) {
+        subscriptionHelper.add(RxHelper.addSugar(api.postBetODZRecordList(HGConstant.PRODUCT_PLATFORM, gtype, Checked, Cancel, date_start, date_end, page))
+                .subscribe(new ResponseSubscriber<AppTextMessageResponse<BetRecordResult>>() {
+                    @Override
+                    public void success(AppTextMessageResponse<BetRecordResult> response) {
+                        if (response.isSuccess()) {
+                            view.postBetRecordResult(response.getData());
+                        } else {
+                            view.showMessage(response.getDescribe());
+                        }
+                    }
+
+                    @Override
+                    public void fail(String msg) {
+                        if (null != view) {
+                            view.setError(0, 0);
+                            view.showMessage(msg);
+                        }
+                    }
+                }));
+    }
+
+    @Override
     public void start() {
 
     }
